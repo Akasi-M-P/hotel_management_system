adc2966a115ae69beeae9a0e95cbb6fb1e1d1e7f
Peter Akasi Mba <awintechworld@gmail.com>
2024-10-14 11:06:32 +0000

    worked on styled component props and the css function

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 function App() {
 
   const StyledApp = styled.div`
-    background-color: grey;
+    /* background-color: grey; */
     padding: 20px;
   `
 
@@ -15,12 +16,26 @@ function App() {
     <>
     <GlobalStyles />
     <StyledApp>
+      <Row>
+      <Row type="horizontal">
+        
       <Heading as="h1">The Wild Oasis</Heading>
+      <div>
       <Heading as="h2">Check in and out</Heading>
       <Button onClick={() => alert("Check in")}>Check in</Button>
-      <Button onClick={() => alert("Check out")}>Check out</Button>
+      <Button variation="secondary" size="small" onClick={() => alert("Check out")}>Check out</Button>
+        </div>
+      </Row>
+
+      <Row>
       <Heading as="h3">Form</Heading>
+      <form>
       <Input type="number" placeholder="Number of guests"/>
+      <Input type="number" placeholder="Number of guests"/>
+      </form>
+      </Row>
+
+      </Row>
     </StyledApp>
     </>
   )
