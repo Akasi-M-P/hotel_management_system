ce6fa6a741e33256478e504ee9fb4366cbcfadb9
Peter Akasi Mba <awintechworld@gmail.com>
2024-10-15 21:45:05 +0000

    created pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,28 @@
-import styled from "styled-components"
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import { BrowserRouter, Router, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-
-  const StyledApp = styled.div`
-    /* background-color: grey; */
-    padding: 20px;
-  `
-
   return (
-    <>
-    <GlobalStyles />
-    <StyledApp>
-      <Row>
-      <Row type="horizontal">
-        
-      <Heading as="h1">The Wild Oasis</Heading>
-      <div>
-      <Heading as="h2">Check in and out</Heading>
-      <Button onClick={() => alert("Check in")}>Check in</Button>
-      <Button variation="secondary" size="small" onClick={() => alert("Check out")}>Check out</Button>
-        </div>
-      </Row>
-
-      <Row>
-      <Heading as="h3">Form</Heading>
-      <form>
-      <Input type="number" placeholder="Number of guests"/>
-      <Input type="number" placeholder="Number of guests"/>
-      </form>
-      </Row>
-
-      </Row>
-    </StyledApp>
-    </>
+    <BrowserRouter>
+    <Routes>
+    <Router path="dashboard" element={<Dashboard />}/>
+    <Router path="bookings" element={<Bookings />}/>
+    <Router path="cabins" element={<Cabins />}/>
+    <Router path="users" element={<Users />}/>
+    <Router path="settings" element={<Settings />}/>
+    <Router path="account" element={<Account />}/>
+    <Router path="login" element={<Login />}/>
+    <Router path="*" element={<PageNotFound />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
